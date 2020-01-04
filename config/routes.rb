5a5d0f0822d6131db56000a78ef2c2f250211075
Texnar13 <m-ivan-2000@mail.ru>
2020-01-05 00:13:58 +0300

    lab_9_ended

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'start_page#index'
 
 
-  get 'result' => 'start_page#result'
+  get 'result' => 'start_page#result', :as => 'result'
   #resources :my_result_page
 end
