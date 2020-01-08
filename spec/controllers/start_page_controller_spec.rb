74d2cfc2e92a41a22a49ee94ce9196dd0188cba2
Texnar13 <m-ivan-2000@mail.ru>
2020-01-08 17:30:03 +0300

    fin

---
+++ spec/controllers/start_page_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+
+RSpec.describe StartPageController, type: :controller do
+  context 'app behaviour if user not register' do
+    it 'should redirect to signin page if user do not auth' do
+      get  :index
+      expect(response).to redirect_to('/login')
+      expect(response).to have_http_status(302)
+    end
+  end
+
+end
--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe UsersController, type: :routing do
     end
 
     it "routes to #new" do
-      expect(:get => "/users/new").to route_to("users#new")
+      expect(:get => "/signup").to route_to("users#new")
     end
 
     it "routes to #show" do
@@ -23,16 +23,12 @@ RSpec.describe UsersController, type: :routing do
       expect(:post => "/users").to route_to("users#create")
     end
 
-    it "routes to #update via PUT" do
-      expect(:put => "/users/1").to route_to("users#update", :id => "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(:patch => "/users/1").to route_to("users#update", :id => "1")
+    it "routes to #update via POST" do
+      expect(:post => "/users/1/update").to route_to("users#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/users/1").to route_to("users#destroy", :id => "1")
+      expect(:get => "/users/1/destroy").to route_to("users#destroy", :id => "1")
     end
   end
 end
